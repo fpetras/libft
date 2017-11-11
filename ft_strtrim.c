b90941d89221f57111d1865cb9644623e196f21b
Fabian PETRAS <fpetras@e1r9p7.42.fr>
2017-11-11 09:53:35 +0100

    Modification to make ft_strtrim work with an all blank string

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 11:56:24 by fpetras           #+#    #+#             */
-/*   Updated: 2017/11/10 09:49:18 by fpetras          ###   ########.fr       */
+/*   Updated: 2017/11/10 13:28:42 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,14 @@ char	*ft_strtrim(char const *s)
 
 	i = 0;
 	start = 0;
+	if (!s)
+		return (NULL);
 	end = ft_strlen(s);
-	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
+	while (s[start] && ft_isspace(s[start]))
 		start++;
-	while (s[end - 1] == ' ' || s[end - 1] == '\n' || s[end - 1] == '\t')
+	while (start < end && s[end - 1] && ft_isspace(s[end - 1]))
 		end--;
-	trimmed = (char*)malloc(sizeof(char) * ((end - start) + 1));
-	if (trimmed == NULL)
+	if (!(trimmed = (char*)malloc(sizeof(char) * ((end - start) + 1))))
 		return (NULL);
 	while (i < (end - start))
 	{
