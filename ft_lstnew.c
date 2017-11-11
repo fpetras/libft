2c92c90ad6adc628d41e9e9314432bc5037be2bd
Fabian PETRAS <fpetras@e1r9p7.42.fr>
2017-11-11 09:55:25 +0100

    Creation of every bonus function

---
+++ ft_lstnew.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 10:12:07 by fpetras           #+#    #+#             */
+/*   Updated: 2017/11/11 09:15:17 by fpetras          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list *link;
+
+	link = (t_list*)malloc(sizeof(t_list));
+	if (link == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		link->content = NULL;
+		link->content_size = 0;
+		return (link);
+	}
+	link->content = ft_memalloc(content_size);
+	link->content = ft_memcpy(link->content, content, content_size);
+	link->content_size = content_size;
+	return (link);
+}
